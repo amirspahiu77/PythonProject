406105ed5fa2172b1cc06d396977cc315be573da
amirspahiu77 <spahiuamir10@gmail.com>
2025-10-14 18:50:25 +0200

    Add files via upload

---
+++ PythonAdvanced-AmirSpahiu/module_14/bar_chart.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('avgIQpercountry.csv')
+
+filtered_df = df[df["Average IQ"] >= 100]
+
+filtered_df = filtered_df.sort_values(by="Average Iq", ascending=False)
+
+print(filtered_df)
+
+bars = plt.bar(filtered_df["Cpuntry"], filtered_df["Average IQ"], color="skyblue")
+
+plt.title("Average IQ by Country (IQ >= 100)", fontsize=16)
+
+plt.xlabel("Country", fontsize=14)
+plt.ylabel("Average IQ", fontsize=10)
+
+plt.xticks(rotation-90, fontsize=10)
+plt.yticks(fontsize=10)
+plt.grid(axis='y', linestyle='--', alpha=0.8)
+
+plt.bar_label(bars, fmt='%.2f', fontsize=10, color='black')
+
+plt.tight_layout()
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PythonAdvanced-AmirSpahiu/module_14/scatter_plot.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("avgIQpercountry.csv")
+
+plt.figure(figsize=(10,6))
+
+plt.scatter(df['Mean years of schooling - 2021'], df['Average IQ'], color='purple', alpha=0.7)
+
+plt.xlabel('Scatter Plot of mean Years of schooling vs Average IQ')
+
+plt.ylabel('Average IQ')
+
+plt.grid(True, linestyle='--', alpha=0.7)
+
+plt.show()
