61d0f3581b75eac4964151161b1577b0b4f0d26e
amirspahiu77 <spahiuamir10@gmail.com>
2025-10-23 18:57:24 +0200

    Add files via upload

---
+++ PythonAdvanced-AmirSpahiu/module_16/calculator_app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+
+def calculate(num1, num2, operation):
+    if operation == "Addition":
+        result = num1 + num2
+    elif operation == "Subtraction":
+        result == num1 - num2
+    elif operation == "Multiplication":
+        result = num1 * num2
+    elif operation == "Division":
+        try:
+            result = num1 / num2
+        except ZeroDivisionError:
+            result = "Cannot divide by zero"
+            return result
+
+def main():
+    st.title("Simple Calculator")
+    num1 = st.number_input("enter the first number", step=1)
+    num2 = st.number_input("enter the second number", step=1)
+
+    operation = st.radio("Select operation", ["Addition", "Subtraction", "Multiplication", "Division"])
+
+    result = calculate(num1, num2, operation)
+
+    st.write(f"Result of the {operation} of {num1} and {num2} is: {result}")
+
+    if __name__ == "__main__":
+        main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
