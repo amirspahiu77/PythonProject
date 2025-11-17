d270c070e493aaf57e4116efd3311c7b11f83f57
amirspahiu77 <spahiuamir10@gmail.com>
2025-11-17 19:07:20 +0100

    Add files via upload

---
+++ PythonAdvanced-AmirSpahiu/module_21/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+
+app = FastAPI.FastAPI()
+
+# @app.get("/items/item_id")
+# def read_item(item_id: int):
+#    return {"item_id": item_id}
+
+# @app.get("/")
+# def root():
+#     return {"message": "Hello World"}
+#
+# #path parameters
+# @app.get("/user/{user_id}")
+# def get_user(user_id: int):
+#     return {"user_id": user_id, "name": "John Doe"}
+#
+# #Query parameters
+# @app.get("/items/")
+# def get_items(skip: int = 0, limit: int = 100):
+#     return {"skip": skip, "limit": limit}
+
+@app.get("/items")
+def read_items():
+    return {"item1": ["item1", "item2", "item3"]}
+
+#POST Method
+@app.post("/items/")
+def create_item(name: str, price: float):
+    return {"item_name": name, "price": price}
+
+#PUT Method
+@app.put("/items/{item_id")
+def update_item(item_id: int, name: str, price: float):
+    return {"item_id": item_id, "item_name": name, "price": price}
+
+#DELETE Method
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    return {"message": f"Item {item_id} deleted"}
