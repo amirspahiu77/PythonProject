41d3751b8fe667b9d2091d6b50dfc29edba1b76d
amirspahiu77 <spahiuamir10@gmail.com>
2025-11-24 18:36:14 +0100

    Add files via upload

---
+++ PythonAdvanced-AmirSpahiu/module_22/models.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel, FieldSerializationInfo, field_validator, ValidationInfo
+
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+    email: str
+
+    @field_validator('age')
+    def age_must_be_positive(cls, v, info, ValidationInfo):
+        if v <= 0:
+            raise ValueError('Age must be positive')
+        return v
+
+try:
+    user = User(id=1, name='Alice', age=-1)
+except ValueError as e:
+    print(e)
+
+#Nested Models
+
+class Address(BaseModel):
+    street: str
+    city: str
+
+class User(BaseModel):
+    id: int
+    name: str
+    address: Address
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
