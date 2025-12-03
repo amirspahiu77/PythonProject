d24b923ac6d956eede88d791f007a1379890707e
amirspahiu77 <spahiuamir10@gmail.com>
2025-12-03 19:03:31 +0100

    Add files via upload

---
+++ PythonAdvanced-AmirSpahiu/module_24/database.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+connection = sqlite3.connect('example.db')
+
+# Create a cursor to interact with the database
+cursor = connection.cursor()
+
+#Create a table named 'employees'
+cursor.execute('''
+CREATE TABLE IF NOT EXIST employees(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    position TEXT NOT NULL,
+    department TEXT NOT NULL,
+    salary REAL
+    
+)
+''')
+connection.commit()
+
+#Insert a new employee
+cursor.execute('''
+INSERT INTO employees (name. position, department, salary)
+VALUES (?, ?, ?, ?)
+''', ('John Doe', 'Software Engineer', 'IT', 2000))
+
+connection.commit()
+
+cursor.execute('SELECT * FROM employees')
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+cursor.execute('''
+UPDATE employees
+SET salary = ?
+WHERE name = ?
+''', (3000, 'John Doe'))
+
+connection.commit()
+
+cursor.execute('''
+DELETE FROM employees
+WHERE name = ?
+''', ('John Doe'))
+
+connection.commit()
+
+cursor.close()
+connection.close()
+
+
+
+
+
+
